a8a687a9f96ca195cac14f558cd7fec1643da6cd
Mikhail Kamorin <m.v.kamorin@gmail.com>
2023-06-02 17:25:13 +0300

    Fix dependencies

--- src/Domain/Query/GetFeed/Handler.php
+++ src/Domain/Query/GetFeed/Handler.php
@@ -2,20 +2,27 @@
 
 namespace App\Domain\Query\GetFeed;
 
-use FeedBundle\Service\FeedService;
+use Doctrine\ORM\EntityManagerInterface;
+use FeedBundle\Entity\Feed;
 use Symfony\Component\Messenger\Handler\MessageHandlerInterface;
 
 class Handler implements MessageHandlerInterface
 {
     public function __construct(
-        private readonly FeedService $feedService,
+        private readonly EntityManagerInterface $entityManager,
     ) {
     }
 
     public function __invoke(GetFeedQuery $query): GetFeedQueryResult
     {
-        return new GetFeedQueryResult(
-            $this->feedService->getFeed($query->getUserId(), $query->getCount())
-        );
+        $feedRepository = $this->entityManager->getRepository(Feed::class);
+        $feed = $feedRepository->findOneBy(['readerId' => $query->getUserId()]);
+        if ($feed === null) {
+            $tweets = [];
+        } else {
+            $tweets = array_slice($feed->getTweets(), -$query->getCount());
+        }
+
+        return new GetFeedQueryResult($tweets);
     }
 }
